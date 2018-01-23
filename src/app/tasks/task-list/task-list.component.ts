11130b1c62ef4366793b2334f5e9215b3128c84b
dkrupnou <dzmitry.krupnou@gmail.com>
2018-01-23 14:30:33 +0300

    Tasks 01-08 were completed

---
+++ src/app/tasks/task-list/task-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Task } from './../../models/task';
+import { TaskArrayService } from './../services/task-array.service';
+
+@Component({
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+  tasks: Array<Task>;
+
+  constructor(
+    private taskArrayService: TaskArrayService) { }
+
+  ngOnInit() {
+    console.log(this.tasks);
+    this.getTasks().catch(err => console.log(err));
+  }
+
+  completeTask(task: Task): void {
+    this.taskArrayService.completeTask(task);
+  }
+
+  editTask(task: Task): void {
+
+  }
+
+  private async getTasks() {
+    this.tasks = await this.taskArrayService.getTasks();
+  }
+}
